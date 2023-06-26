ab5973496ab95caef58920738a1193f289e7c424
Gustavo Sales <gussales14@gmail.com>
2023-06-26 11:04:09 -0300

    Update AppContext.ts

--- src/context/AppContext.ts
+++ src/context/AppContext.ts
@@ -1,7 +1,14 @@
 import { createContext } from 'react'
 
 import { Value } from '../@types/ContextType'
-const INITIAL_STATE: Value = {}
+const INITIAL_STATE: Value = {
+  isLoading: false,
+  products: [],
+  showModal: false,
+  setShowModal: () => false,
+  setIsLoading: () => false,
+  setProducts: () => [],
+}
 
 const AppContext = createContext(INITIAL_STATE)
 
